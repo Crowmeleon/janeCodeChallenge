112e8d7a22a1fd85b446d93b4e7fe1b87c2dd723
petermartinez225@gmail.com <peter@thriftmatch.com>
2018-07-29 14:59:24 -0700

    Fixed bug with Route

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ module.exports = app => {
         });
     });
 
-    app.get('*', (res, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
       });
 }; 
