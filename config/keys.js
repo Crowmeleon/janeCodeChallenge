6f2073bacdbae6aabb84e8cebe77975efb7fce21
petermartinez225@gmail.com <peter@thriftmatch.com>
2018-08-01 09:19:23 -0700

    Updated the keys to not show the development mongodb connection string

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-module.exports = {
-    mongoDB: 'mongodb://Admin:Boomerdog225@ds259001.mlab.com:59001/jane'
-}
-
-// figure out what set of credentials to return 
-
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod');
     }else{
